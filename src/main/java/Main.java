44bf3d97466783039061ef7a37762fa1f5c41bc0
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-10-26 16:42:59 -0400

    Alice and Bob say hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
+        //inquiring the name to lead to output
+        System.out.println("What is your name?");
 
+        String name = in.nextLine();
+//set alice and bob to both be acceptable answers, regardless of case sensitivity, to output "Hello!" if it
+        // matches either, and to output a different message if the names do not match either Bob or Alice.
+        if(name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+            System.out.print("Hello!");
+        } else {
+            System.out.print("You shouldn't be here");
+        }
     }
 }
